eb83f896c3d4ca916d54efd136b1c3a1d292d54d
Venish22 <110281437+Venish22@users.noreply.github.com>
2024-03-06 13:51:36 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -27,7 +27,7 @@ cd Week-2_Azure_CloudOps/backend
 
 cat > .env <<EOF
 # Content of .env file
-DB_HOST=demopocsqlweee.database.windows.net
+DB_HOST=demopocsqlwecan.database.windows.net
 DB_USERNAME=azureuser
 DB_PASSWORD=password@123@9(&)
 PORT=3306
