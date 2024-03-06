ccf3c0ba32989b5aa3a325c349512ca33aa891e3
Venish22 <110281437+Venish22@users.noreply.github.com>
2024-03-06 14:37:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,9 @@
-  #!/bin/bash
+#!/bin/bash
 
-  sudo apt update -y
-
-  sudo apt install apache2 -y
-
-  curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
-  sudo apt-get install -y nodejs -y
-
-  sudo apt update -y
-
-  sudo npm install -g corepack -y
-
-  corepack enable
-
-  corepack prepare yarn@stable --activate --yes
-
-  sudo yarn global add pm2
-
+chmod u+x setup.sh
+./setup.sh
+git clone https://github.com/Joelayo/Week-2_Azure_CloudOps.git
+cd Week-2_Azure_CloudOps/client
+npm install
+npm run build
+sudo cp -r build/* /var/www/html
