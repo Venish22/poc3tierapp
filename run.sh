caf69d6797b9136364ab858f6fc94b131e9b5a86
Venish22 <110281437+Venish22@users.noreply.github.com>
2024-03-06 13:48:47 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-app_id = "8da73450-e5be-4053-ba0e-7c22dd1cce49"
-password = "Nv88Q~ARXpyljNIt8f_p1vL.VjWtnGB0dzh9Kcge"
-tenant_id = "4da5ce08-e277-4580-b338-7cf1b5e7e3ba"
-
 chmod u+x setup.sh
 ./setup.sh
 git clone https://github.com/Joelayo/Week-2_Azure_CloudOps.git
@@ -17,18 +13,23 @@ chmod u+x setup1.sh
 ./setup1.sh
 
 #azlogin using service principle
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-az login --service-principal -u ${app_id} -p ${password} --tenant ${tenant_id}
 
-dbhost = az mysql server show --resource-group <resource-group-name> --name <mysql-server-name> --query fullyQualifiedDomainName -o tsv
+# app_id = "8da73450-e5be-4053-ba0e-7c22dd1cce49"
+# password = "Nv88Q~ARXpyljNIt8f_p1vL.VjWtnGB0dzh9Kcge"
+# tenant_id = "4da5ce08-e277-4580-b338-7cf1b5e7e3ba"
+
+# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# az login --service-principal -u ${app_id} -p ${password} --tenant ${tenant_id}
+
+# dbhost = az mysql server show --resource-group {0} --name {1} --query fullyQualifiedDomainName -o tsv
 
 cd Week-2_Azure_CloudOps/backend
 
 cat > .env <<EOF
 # Content of .env file
-DB_HOST=${dbhost}
+DB_HOST=demopocsqlweee.database.windows.net
 DB_USERNAME=azureuser
-DB_PASSWORD="<DATABASE_PASSWORD>"
+DB_PASSWORD=password@123@9(&)
 PORT=3306
 EOF
 npm install
