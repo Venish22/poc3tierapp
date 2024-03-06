8566083b027557caedcf4c0e67b299cf2fa515be
Venish22 <110281437+Venish22@users.noreply.github.com>
2024-03-06 14:39:56 +0530

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -1,19 +1,16 @@
-  #!/bin/bash
+#!/bin/bash
 
-  sudo apt update -y
-
-  sudo apt install apache2 -y
-
-  curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
-  sudo apt-get install -y nodejs -y
-
-  sudo apt update -y
-
-  sudo npm install -g corepack -y
-
-  corepack enable
-
-  corepack prepare yarn@stable --activate --yes
-
-  sudo yarn global add pm2
-
+#App Tier
+chmod u+x setup1.sh
+./setup1.sh
+cd Week-2_Azure_CloudOps/backend
+cat > .env <<EOF
+# Content of .env file
+DB_HOST=demopocsqleeweedo.database.windows.net
+DB_USERNAME=azureuser
+DB_PASSWORD="password@123@9(&)"
+PORT=3306
+EOF
+npm install
+npm install dotenv
+sudo apt install mysql-server -y
