f1cf3d78906953410478d6adb0f6790bbf3a3d3e
Venish22 <110281437+Venish22@users.noreply.github.com>
2024-03-06 15:30:25 +0530

    Update apptier_setup.sh

--- apptier_setup.sh
+++ apptier_setup.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-#App Tier
+# App Tier
 sudo apt update -y
-
 sudo apt install apache2 -y
-
 sudo apt install git
 
 sudo dpkg --configure -a
 sudo apt install -f
 sudo apt update
 
+# Install Node.js and npm
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs npm
 
 sudo apt update -y
 
-sudo npm install -g corepack -y
-
+# Install corepack
+sudo npm install -g corepack
 corepack enable
-
 corepack prepare yarn@stable --activate --yes
 
+# Install yarn
+sudo npm install -g yarn
+
+# Install pm2
 sudo yarn global add pm2
 
+# Clone repository
 git clone https://github.com/Joelayo/Week-2_Azure_CloudOps.git
+
+cd Week-2_Azure_CloudOps/backend
 
 cd Week-2_Azure_CloudOps/backend
 cat > .env <<EOF
