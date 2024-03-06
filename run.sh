7cd22b135bcf279634b9eca9a6779792f763b667
Venish22 <110281437+Venish22@users.noreply.github.com>
2024-03-06 13:59:40 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-sudo apt install nodejs
+  sudo apt update -y
+
+  sudo apt install apache2 -y
+
+  curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+  sudo apt-get install -y nodejs -y
+
+  sudo apt update -y
+
+  sudo npm install -g corepack -y
+
+  corepack enable
+
+  corepack prepare yarn@stable --activate --yes
+
+  sudo yarn global add pm2
 
 chmod u+x setup.sh
 ./setup.sh
