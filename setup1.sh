185869a8e877e46a830e02c9132da77d76009201
Venish22 <110281437+Venish22@users.noreply.github.com>
2024-03-06 14:44:20 +0530

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 #App Tier
-chmod u+x setup1.sh
-./setup1.sh
+sudo apt update -y
+
+sudo apt install apache2 -y
+
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs -y
+
+sudo apt update -y
+
+sudo npm install -g corepack -y
+
+corepack enable
+
+corepack prepare yarn@stable --activate --yes
+
+sudo yarn global add pm2
+
+git clone https://github.com/Joelayo/Week-2_Azure_CloudOps.git
+
 cd Week-2_Azure_CloudOps/backend
 cat > .env <<EOF
 # Content of .env file
